a835f9661a9057d6d2097994b8b2bbfa9ab9664a
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-02-07 21:50:28 +0300

    Added admin panel, added rest framework, started views

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Women
+
 
 # Create your views here.
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
